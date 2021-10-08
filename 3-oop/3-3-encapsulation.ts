638fd0bc1d047a9d8a7a18bf3f12643568244432
jinwook-song <wlsdnr129@naver.com>
2021-10-08 21:44:38 +0900

    lecture 4. Getter & Setter

--- 3-oop/3-3-encapsulation.ts
+++ 3-oop/3-3-encapsulation.ts
@@ -41,4 +41,25 @@
   const maker = CoffeeMaker.makeMachine(32);
   maker.fillCoffeeBeans(24);
   console.log(maker);
+
+  //////////////////////////////////////////////////////////////////////
+
+  class User {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num < 0) {
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, public lastName: string) {}
+  }
+  const user = new User("Steve", "Jobs");
+  user.age = 6;
+  console.log(user.fullName);
 }
