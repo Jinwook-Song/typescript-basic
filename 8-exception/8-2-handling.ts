90f679d1e5251d5c1f15b5b6076f451922e08136
jinwook-song <wlsdnr129@naver.com>
2021-10-12 22:38:39 +0900

    lecture 9. 처리할 수 있는 곳에서 에러 처리

---
+++ 8-exception/8-2-handling.ts
@@ -0,0 +1,31 @@
+class NetworkClient {
+  tryConnect(): void {
+    throw new Error("no network.");
+  }
+}
+
+class UserService {
+  constructor(private client: NetworkClient) {}
+
+  login() {
+    this.client.tryConnect();
+    // login...
+  }
+}
+
+class App {
+  constructor(private userService: UserService) {}
+  run() {
+    // 처리할 수 있는 곳에서 에러 처리하기
+    try {
+      this.userService.login();
+    } catch (error) {
+      // show dialog to user
+    }
+  }
+}
+
+const client = new NetworkClient();
+const service = new UserService(client);
+const app = new App(service);
+app.run();
